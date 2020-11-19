9e6c8c337de357ace3ed20d3c28b43f7d35ad793
Peter Wagner <git@mycloudand.me>
2020-11-19 09:40:12 -0500

    action from config

--- action/handlers.go
+++ action/handlers.go
@@ -2,27 +2,24 @@ package action
 
 import (
 	"context"
+	"strings"
 
-	"github.com/google/go-github/v32/github"
 	"github.com/thepwagner/action-update/actions"
 )
 
 func NewHandlers(env *Environment) *actions.Handlers {
 	return &actions.Handlers{
 		WorkflowDispatch: func(ctx context.Context) error {
-			gh, err := env.NewTokensClient()
+			tc, err := env.NewTokensClient()
 			if err != nil {
 				return err
 			}
 
-			// FIXME: config from input
-			token, err := gh.Mint(ctx, []string{"thepwagner-org/private"}, &github.InstallationPermissions{
-				Contents: github.String("read"),
-			})
-			if err != nil {
-				return err
-			}
-			return gh.StoreRepo(ctx, "thepwagner-org", "secret-garden", "TEST_TOKEN", token)
+			repoSplit := strings.SplitN(env.GitHubRepository, "/", 2)
+			org := repoSplit[0]
+
+			c := NewConfigProcessor(org, tc, tc)
+			return c.ProcessConfig(ctx, "config/")
 		},
 	}
 }
